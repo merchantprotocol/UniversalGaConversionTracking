2657a288504c400b6fac222473b8b2478cc75015
Jonathon Byrd <jonathonbyrd@gmail.com>
2016-12-02 18:19:59 -0800

    added in array

--- app/code/community/Interactiv4/GAConversionTrack/Model/Observer.php
+++ app/code/community/Interactiv4/GAConversionTrack/Model/Observer.php
@@ -31,7 +31,7 @@ class Interactiv4_GAConversionTrack_Model_Observer
             $qty = 0;
             $isRFQS = false;
             foreach ($order->getAllVisibleItems() as $item) {
-            	if ($item->getSku()!=='RF_QS') {
+            	if (!in_array($item->getSku(),array('RF_QS','RF_QSNAS'))) {
             		continue;
             	}
             	$isRFQS = true;
